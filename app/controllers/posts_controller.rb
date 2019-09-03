21c050f2f2940b06344eee5b5339238ba3d6c7a5
tresagua0123 <gentlekai10@gmail.com>
2019-09-04 03:09:45 +0900

    geocode pending problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,8 @@ class PostsController < ApplicationController
     else 
       redirect_back(fallback_location: root_path)
     end
+
+
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,16 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
-
-  after_validation :geocode
+  validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, :if => lambda{ |obj| obj.address_changed? } 
 
   private
   def geocode
     uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=AIzaSyD1-AY1ENg8eR_6of_ZMKKt8MPcVqxyynE")
     res = HTTP.get(uri).to_s
     response = JSON.parse(res)
+
     self.latitude = response["results"][0]["geometry"]["location"]["lat"]
     self.longitude = response["results"][0]["geometry"]["location"]["lng"]
   end
