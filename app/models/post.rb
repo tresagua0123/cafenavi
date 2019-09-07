e1d6552cf0a2406c25daee2aaf705c3998c82d39
tresagua0123 <gentlekai10@gmail.com>
2019-09-07 21:01:38 +0900

    delete fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_one_attached :image
 
   belongs_to :user
   belongs_to :prefecture
 
-  has_many :post_tags 
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   has_many :likes, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   has_one_attached :avatar
   # Include default devise modules. Others available are:
