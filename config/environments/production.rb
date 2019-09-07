2193a881b138d75d8bf8d4019717220c541ebe0b
tresagua0123 <gentlekai10@gmail.com>
2019-09-08 00:06:12 +0900

    hopefully error solved?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.require_master_key = true
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
