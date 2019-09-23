7d5dca5b205e9748ef33d30d1ca3e9ebba9f05a6
tresagua0123 <gentlekai10@gmail.com>
2019-09-23 21:33:09 +0900

    ajax based like added

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+
+    before_action :set_variables, only:[:like, :unlike]
     def create
         @like = current_user.likes.create(post_id: params[:post_id])
         redirect_back(fallback_location: root_path)
@@ -7,5 +9,26 @@ class LikesController < ApplicationController
         @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
         @like.destroy 
         redirect_back(fallback_location: root_path)
+    end
+
+    #added the codes below newly on 9/17
+
+    def like
+        like = current_user.likes.new(post_id: @post.id)
+        like.save
+    end
+
+    def unlike
+        like = current_user.likes.find_by(post_id: @post.id)
+        like.destroy
+    end
+
+    private
+
+    def set_variables
+        @post = Post.find(params[:post_id])
+        @id_name = "#like-link-#{@post.id}" 
+        @id_heart = "#heart-#{@post.id}"
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
  
   root 'posts#index'
   get '/about', to: 'pages#about'
+
+  post '/likes/:post_id', to: 'likes#like', as: 'like'
+  delete '/likes/:post_id', to: 'likes#unlike', as: 'unlike'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
